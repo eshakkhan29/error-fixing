9fd4bd2b4bfe244d9d165cba073b78e126d9ae34
eshakkhan29 <eshakkhan29@gmail.com>
2022-03-10 22:37:23 +0600

    fix show report and liked

--- js/app.js
+++ js/app.js
@@ -144,6 +144,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).textContent = '';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -152,8 +153,9 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+    document.getElementById( "reported" ).textContent = '';
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
